c998007464cc4ec5b589a26251b8218f766df80c
Leon <leon.zampariolo@gmail.com>
2022-06-06 21:13:05 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pyautogui as pg
+import time
+import keyboard as k
+
+if k.read_key() == 's':
+  for i in range(20):
+    pg.leftClick()
+    pg.leftClick()
+    pg.leftClick()
+    pg.leftClick()
+    pg.leftClick()
+    pg.leftClick()
+    pg.leftClick()
